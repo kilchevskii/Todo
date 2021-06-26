d22533e311353397af751940824387738723bd12
Vlad <you@example.com>
2021-06-26 11:41:17 +0300

    todo app

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as AddList } from '../components/AddList'
+export { default as Tasks } from '../components/Tasks'
+export { default as List } from '../components/List'
+export { default as Badge } from '../components/Badge'
+
+// export {default as  AddList} from './components/AddList'
